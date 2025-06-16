ad8df07a1bcdf5e26229fd03c5917b58f20d3f4b
nandasp23805 <nandasatriaperwira23805@gmail.com>
2025-06-16 15:02:08 +0700

    crt : project

---
+++ script.js
@@ -0,0 +1,66 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+    "https://pcqrwkvardtgkurjugra.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjcXJ3a3ZhcmR0Z2t1cmp1Z3JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5ODkyNjYsImV4cCI6MjA2NTU2NTI2Nn0.SUVJnM82j0WylXBM2Qf7WTjz17xzivwGnoxrzt3k9Uo"
+)
+
+async function simpanResep() {
+    const judul = document.getElementById("judul").value;
+    const alat = document.getElementById("alat").value;
+    const bahan = document.getElementById("bahan").value;
+    const steps = document.getElementById("steps").value;
+
+    const { data, error } = await supabase
+        .from('resep')
+        .insert([
+            { judul: judul, alat: alat, bahan: bahan, steps: steps }
+        ])
+
+    if (error) return alert("Gagal menambahkan resep: " + error.message)
+    alert("Resep berhasil ditambahkan!");
+
+    loadData();
+}
+
+async function loadData() {
+  const { data, error } = await supabase.from("resep").select("*");
+  if (error) return alert("Data gagal diambil: " + error.message);
+
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+
+  if (data.length === 0) {
+    container.innerHTML = `
+      <div class="col-12 text-center">
+        <img src="404.png" alt="No Recipes" class="img-fluid mb-3" style="max-width: 250px;">
+        <p class="text-muted">Tidak ada resep tersedia.</p>
+      </div>
+    `;
+    return;
+  }
+
+  data.forEach((item) => {
+    const col = document.createElement("div");
+    col.className = "col";
+    col.innerHTML = `
+      <div class="card bg-card border border-secondary shadow">
+        <div class="card-body">
+          <h5 class="card-title">${item.nama_barang}</h5>
+          <p class="card-text">Alat: ${item.alat || "-"}</p>
+          <p class="card-text">Bahan: ${item.bahan || "-"}</p>
+          <p class="card-text">Langkah: ${item.deskripsi || "-"}</p>
+          <div class="d-flex justify-content-between mt-3">
+            <button onclick="confirmHapus(${item.id})"
+              class="px-3 py-1 bg-red-500 hover:bg-red-600 text-white rounded text-sm">🗑️ Hapus</button>
+          </div>
+        </div>
+      </div>
+    `;
+    container.appendChild(col);
+  });
+}
+
+
+window.simpanResep = simpanResep;
+loadData();
